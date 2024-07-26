3eaf215f4236d8b32a9b928b92f319d0ff896702
Tikam <tikamshah@outlook.com>
2024-07-26 21:27:31 +0530

    wt

--- app/Http/Controllers/Backend/WhatsappmessageController.php
+++ app/Http/Controllers/Backend/WhatsappmessageController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
+
 class WhatsappmessageController extends Controller
 {
     public function index(): Renderable
